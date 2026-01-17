fc5871e9397b31d1fdd25194d0807f2931ae5796
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-01-18 00:30:01 +0700

    use variable

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,10 +2,13 @@ import './style.css'
 
 
 const MyComponent = () => {
+       const name = "Thang";
+       const age = 21;
        return (
               <>
               <div style={{fontStyle: "italic"}}>Đây là một component tự tạo</div>
               <div className='text'>Sử dụng fragment giúp tránh render thừa html</div>
+              <div>Tôi là {name} và tôi {age} tuổi {console.log("Chào mọi người!!!")}</div>
               </>
        );
 }
