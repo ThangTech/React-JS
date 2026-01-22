06cabcdcabbb9ac1815bc6c17bad05ddb25f7318
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-01-22 21:05:48 +0700

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import './components/todo/todo.css'
 import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg'
+import { useState } from 'react';
 
 const App = () => {
+       const [todoList, setTodoList] = useState([
+              {id: 1, name: "Learning React"},
+              {id: 2, name: "Learning TypeScript"}
+       ])
        const name = "Nguyen Quoc Viet";
        const age = 20;
        const hometown = {
@@ -22,7 +27,8 @@ const App = () => {
               <TodoData 
               name={name}
               age={age}
-              hometown={hometown}       
+              hometown={hometown}
+              todoList={todoList}       
               />
               <div className='todo-image'>
                      <img src={reactLogo} className='logo' alt="react" />
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 const TodoNew = (props) => {
        const {addNewTodo} = props;
 
-       addNewTodo("Thang");
+       // addNewTodo("Thang");
+       const [valueInput, setValueInput] = useState("Thắng"); //Khởi tạo giá trị mặc dịnh cho state
+
+       const handleClick = () => {
+              console.log("check value input", valueInput);
+       }
+       const handleOnChange = (name) => {
+              setValueInput(name)
+       }
        return (
               <div className='todo-new'>
-                     <input type="text" placeholder='Enter your task'/>
-                     <button>Add</button>
+                     <input type="text" placeholder='Enter your task'
+                     onChange={() => handleOnChange(event.target.value)}
+                     />
+                     <button style={{cursor: "pointer"}} 
+                     onClick={handleClick}
+                     >Add</button>
+                     <div>My name is {valueInput}</div>
               </div>
        )
 }
