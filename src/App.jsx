5407750e01e821e3a08eb2a559c198bc33168216
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-01-23 00:32:50 +0700

    render with condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,65 @@
-import './components/todo/todo.css'
-import TodoData from './components/todo/TodoData';
-import TodoNew from './components/todo/TodoNew';
-import reactLogo from './assets/react.svg'
-import { useState } from 'react';
+import "./components/todo/todo.css";
+import TodoData from "./components/todo/TodoData";
+import TodoNew from "./components/todo/TodoNew";
+import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 
 const App = () => {
-       const randomIntInterval = (min, max) => {
-              return Math.floor(Math.random()*(max-min+1) + min);
-       }
-       const [todoList, setTodoList] = useState([
-              // {id: 1, name: "Learning React"},
-              // {id: 2, name: "Learning TypeScript"}
-       ])
-       // const name = "Nguyen Quoc Viet";
-       // const age = 20;
-       // const hometown = {
-       //        commune: "An Tao",
-       //        province: "Hung Yen"
-       // }
-       const addNewTodo = (name) => {
-              const newTodo = {
-                     id: randomIntInterval(3, 20),
-                     name: name
-              }
-              setTodoList([...todoList, newTodo])
-       }
+  const randomIntInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  const [todoList, setTodoList] = useState([
+    // {id: 1, name: "Learning React"},
+    // {id: 2, name: "Learning TypeScript"}
+  ]);
+  // const name = "Nguyen Quoc Viet";
+  // const age = 20;
+  // const hometown = {
+  //        commune: "An Tao",
+  //        province: "Hung Yen"
+  // }
+  const addNewTodo = (name) => {
+    const newTodo = {
+      id: randomIntInterval(3, 20),
+      name: name,
+    };
+    setTodoList([...todoList, newTodo]);
+  };
   return (
-  <>
-       <div className="todo-container">
-              <div className="todo-title"><h1>Todo list</h1></div>
-              <TodoNew 
-              addNewTodo={addNewTodo}/>
-              <TodoData 
-              // name={name}
-              // age={age}
-              // hometown={hometown}
-              todoList={todoList}       
-              />
-              <div className='todo-image'>
-                     <img src={reactLogo} className='logo' alt="react" />
-              </div>
-       </div>
-  
-  </>
-)};
+    <>
+      <div className="todo-container">
+        <div className="todo-title">
+          <h1>Todo list</h1>
+        </div>
+        <TodoNew addNewTodo={addNewTodo} />
+
+        {/* {todoList.length > 0 && (
+          <TodoData
+            // name={name}
+            // age={age}
+            // hometown={hometown}
+            todoList={todoList}
+          />
+        )}
+        {todoList.length === 0 && (
+          <div className="todo-image">
+            <img src={reactLogo} className="logo" alt="react" />
+          </div>
+        )} */}
+
+        {todoList.length > 0 ? 
+        (
+          <TodoData todoList={todoList} />
+        ) 
+        : 
+        (
+          <div className="todo-image">
+            <img src={reactLogo} className="logo" alt="react" />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default App;
