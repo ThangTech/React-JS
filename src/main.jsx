7739b97878e8117751eef7619b00cb16ca3e627b
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-01-25 14:25:37 +0700

    config router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+       { 
+              path: "/", 
+              element: <App></App> 
+       },
+       {
+              path: '/login',
+              element: <div>Login</div>
+       },
+       {
+              path: '/register',
+              element: <div>register</div>
+       },
+       {
+              path: '/user',
+              element: <div>user</div>
+       },
+       {
+              path: '/product',
+              element: <div>product</div>
+       }
+]
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
-)
+);
