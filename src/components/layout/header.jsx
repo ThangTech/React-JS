85133981687c698a86b58fcab7719972eea6be7f
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-01-25 16:31:50 +0700

    using 'NavLink'

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,16 +1,17 @@
 import "../../style/layout/header.css"
-import {Link} from "react-router"; //Link de chuyen huong giua cac trang khac nhau ma khong can tai lai trang
+import {Link, NavLink} from "react-router"; //Link de chuyen huong giua cac trang khac nhau ma khong can tai lai trang
+//NavLink de biet duoc trang nao dang duoc chon và co the them class active
 const Header = () => {
   return (
       <ul>
         <li>
-          <Link to="/" className="active">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <Link to="/user">Users</Link>
+          <NavLink to="/user">Users</NavLink>
         </li>
         <li>
-          <Link to="/product">Products</Link>
+          <NavLink to="/product">Products</NavLink>
         </li>
       </ul>
   );
