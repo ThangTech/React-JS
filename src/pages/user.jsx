88cb3fac016f6c0f1645e89b2e44390b22d5eb62
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-01-28 14:46:17 +0700

    create gui user and test data

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,8 +1,16 @@
-
+import UserForm from "../components/user/userForm";
+import UserTable from "../components/user/userTable";
+import "../style/user/userForm.css";
 const UserPage = () => {
-       return (
-              
-              <div>user page</div>
-       )
-}
-export default UserPage
+  return (
+    <div className="user-form">
+      <div>
+              <UserForm/>
+       </div>
+      <div>
+        <UserTable />
+      </div>
+</div>
+  );
+};
+export default UserPage;
