9ccccf478a70ff42521d3481af274c6c8f0f0a7d
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-02-27 15:08:07 +0700

    Lifting State Up from userTable and userForm up component user

--- src/components/user/userForm.jsx
+++ src/components/user/userForm.jsx
@@ -1,75 +1,98 @@
 import { Input, Button, notification } from "antd";
 import { useState } from "react";
 import { createUser } from "../../services/api.service";
-import { Modal } from 'antd';
-const UserForm = () => {
+import { Modal } from "antd";
+const UserForm = (props) => { 
+  const { loadUser } = props;
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleSubmit = async() => {
-       setIsModalOpen(true)
-       const res = await createUser(fullName, email, password, phone);
-       if(res.data){
-              notification.success({
-              message: "create user",
-              description: "create user success"
-              })
-              setIsModalOpen(false)       
-       }
-       else{
-             notification.error({
-              message: "Error create user",
-              description: JSON.stringify(res.message)
-             }) 
-       }
-
-
-  }
+  const handleSubmit = async () => { 
+    setIsModalOpen(true);
+    const res = await createUser(fullName, email, password, phone);
+    if (res.data) {
+      notification.success({
+        message: "create user",
+        description: "create user success",
+      });
+      resetModal();
+      await loadUser();
+    } else {
+      notification.error({
+        message: "Error create user",
+        description: JSON.stringify(res.message),
+      });
+    }
+  };
+  const resetModal = () => {
+    setIsModalOpen(false);
+    setFullName("");
+    setEmail("");
+    setPassword("");
+    setPhone("");
+  };
   return (
     <div>
-       <div style={{display: "flex", justifyContent: "space-between"}}>
-              <h3>Table users</h3>
-              <Button type="primary"
-              onClick={() => {setIsModalOpen(true)}}
-              >Create user</Button>
-       </div>
-        <Modal
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h3>Table users</h3>
+        <Button
+          type="primary"
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          Create user
+        </Button>
+      </div>
+      <Modal
         title="Create user"
-        closable={{ 'aria-label': 'Custom Close Button' }}
+        closable={{ "aria-label": "Custom Close Button" }}
         open={isModalOpen}
         onOk={handleSubmit}
-        onCancel={() => {setIsModalOpen(false)}}
+        onCancel={resetModal}
         maskClosable={false}
         okText={"Create"}
       >
         <div>
           <span>Full Name</span>
-          <Input value={fullName}
-          onChange={(event) => {setFullName(event.target.value)}}
-           />
+          <Input
+            value={fullName}
+            onChange={(event) => {
+              setFullName(event.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Email</span>
-          <Input value={email}
-          onChange={(event) => {setEmail(event.target.value)}}
+          <Input
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
           />
         </div>
         <div>
           <span>Password</span>
-          <Input.Password value={password}
-          onChange={(event) => {setPassword(event.target.value)}}
+          <Input.Password
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
           />
         </div>
         <div>
           <span>Phone number</span>
-          <Input value={phone}
-          onChange={(event) => {setPhone(event.target.value)}}
+          <Input
+            value={phone}
+            onChange={(event) => {
+              setPhone(event.target.value);
+            }}
           />
         </div>
       </Modal>
-</div>
+    </div>
   );
 };
 export default UserForm;
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,14 +1,24 @@
 import UserForm from "../components/user/userForm";
 import UserTable from "../components/user/userTable";
 import "../style/user/userForm.css";
+import { useEffect, useState } from "react";
+import { fetchAllUser } from "../services/api.service";
 const UserPage = () => {
+       const [dataUsers, setDataUsers] = useState([]);
+       useEffect(() => {
+              loadUser();
+       }, []);
+       const loadUser = async() => {
+       const res = await fetchAllUser();
+       setDataUsers(res.data)
+  }
   return (
     <div className="user-form">
       <div>
-              <UserForm/>
+              <UserForm loadUser={loadUser}/>
        </div>
       <div>
-        <UserTable />
+        <UserTable dataUsers={dataUsers}/>
       </div>
 </div>
   );
