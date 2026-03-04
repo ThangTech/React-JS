7c0664cc485de5dc30e18a89bd87e535a06ac97c
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-03-05 01:16:05 +0700

    preview image

--- src/components/user/user.detail.jsx
+++ src/components/user/user.detail.jsx
@@ -1,11 +1,25 @@
-import { Drawer } from "antd";
-
+import { Button, Drawer } from "antd";
+import { useState } from "react";
 const ViewModal = (props) => {
   const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
-
+  const [selectedFile, setSelectedFile] = useState()
+  const [preview, setPreview] = useState()
+  const handleOnChangeFile = () => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null)
+            setPreview(null)
+            return;
+        }
+        const file = event.target.files[0];
+        if(file){
+              setSelectedFile(file)
+              setPreview(URL.createObjectURL(file))
+        } 
+    }
   return (
     <>
       <Drawer
+        width={"40vw"}
         title="Chi tiết user"
         closable={{ "aria-label": "Close Button" }}
         onClose={() => {
@@ -22,6 +36,28 @@ const ViewModal = (props) => {
         <p>Email: {dataDetail.email}</p>
         <br />
         <p>Phone: {dataDetail.phone}</p>
+        <br />
+        <p>Avatar:</p>
+        <div>
+             <img 
+             height={200} width={300}
+             src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}/>
+
+        </div>
+        <div>
+              <label htmlFor="btnUpload" style={{padding: "5px", backgroundColor: "green", borderRadius: "50px", color: "white", cursor: "pointer"}}>Upload Avatar</label>
+              <input type="file" hidden id="btnUpload" 
+              onChange={(event) => handleOnChangeFile(event)}/>
+        </div>
+        {preview &&
+        <div>
+             <img 
+             height={200} width={300}
+             src={preview}/>
+
+        </div>
+       }
+        {/* <Button type="primary">Upload Image</Button> */}
         </> : <p>Some thing went wrong...</p>}
       </Drawer>
     </>
